e82b7eeda8a92f49aab0f992e97b20fc6aa47e0c
itamar <itamargomes038@hotmail.com>
2022-05-21 17:20:41 -0300

    commit 01

---
+++ app/src/main/java/com/itamargomes/rabisco/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.itamargomes.rabisco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    //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
+}
